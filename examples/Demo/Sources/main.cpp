8e71d080ddfed4b7ca70b6a074e835d10ce812cd
Liavontsi Khachuyeu <Liavontsi_Khachuyeu@epam.com>
2025-04-12 13:40:55 +0200

    Add Android Demo

--- examples/Demo/Sources/main.cpp
+++ examples/Demo/Sources/main.cpp
@@ -28,7 +28,7 @@ void setupScene(std::shared_ptr<ISceneService> sceneService)
     solid->setScale(Vector3df(35.0f, 1.0f, 15.0f));
     solid->setIsVisualizationEnabled(true);
 
-    for (int i = 1; i < 32; i++)
+    for (int i = 1; i < 10; i++)
     {
         for (int j = 1; j < 32; j++)
         {
