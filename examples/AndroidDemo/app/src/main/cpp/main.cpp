cae4a4281d5180d3c1cafda2e94fa7f6ef3841f4
Liavontsi Khachuyeu <liavontsi_khachuyeu@epam.com>
2025-04-27 22:57:12 +0200

    Fix building for Android

--- examples/AndroidDemo/app/src/main/cpp/main.cpp
+++ examples/AndroidDemo/app/src/main/cpp/main.cpp
@@ -12,6 +12,12 @@ extern "C" {
 
 #include <game-activity/native_app_glue/android_native_app_glue.c>
 
+using namespace TEngine;
+using namespace TEngine::Components::Graphics;
+
+std::shared_ptr<IEngine> engine;
+std::shared_ptr<IGraphicsService> graphicsService;
+
 /*!
  * Handles commands sent to this Android application
  * @param pApp the app the commands are coming from
@@ -19,20 +25,32 @@ extern "C" {
  */
 void handle_cmd(android_app *pApp, int32_t cmd) {
     switch (cmd) {
-        case APP_CMD_INIT_WINDOW:
+        case APP_CMD_INIT_WINDOW: {
             // A new window is created, associate a renderer with it. You may replace this with a
             // "game" class if that suits your needs. Remember to change all instances of userData
             // if you change the class here as a reinterpret_cast is dangerous this in the
             // android_main function and the APP_CMD_TERM_WINDOW handler case.
-            pApp->userData = new Renderer(pApp);
+            engine = createEngine(pApp);
+
+            auto creationParameters = TEngine::Models::createEngineParameters();
+
+            auto graphicsParameters = creationParameters->getGraphicsParameters();
+            graphicsParameters->setTitle("AndroidDemo");
+            graphicsParameters->setIsVerticalSyncEnabled(true);
+            graphicsParameters->getSceneParameters()->setSequenceUpdateThreshold(20.f);
+
+            engine->initialize(creationParameters);
+
+            graphicsService = engine->getGraphicsService();
+            //pApp->userData = new Renderer(pApp);
             break;
+        }
         case APP_CMD_TERM_WINDOW:
             // The window is being destroyed. Use this to clean up your userData to avoid leaking
             // resources.
             //
             // We have to check if userData is assigned just in case this comes in really quickly
             if (pApp->userData) {
-                //
                 auto *pRenderer = reinterpret_cast<Renderer *>(pApp->userData);
                 pApp->userData = nullptr;
                 delete pRenderer;
@@ -62,8 +80,6 @@ bool motion_event_filter_func(const GameActivityMotionEvent *motionEvent) {
  * This the main entry point for a native activity
  */
 void android_main(struct android_app *pApp) {
-    TEngine::
-
     // Can be removed, useful to ensure your code is running
     aout << "Welcome to android_main" << std::endl;
 
@@ -73,7 +89,7 @@ void android_main(struct android_app *pApp) {
     // Set input event filters (set it to NULL if the app wants to process all inputs).
     // Note that for key inputs, this example uses the default default_key_filter()
     // implemented in android_native_app_glue.c.
-    android_app_set_motion_event_filter(pApp, motion_event_filter_func);
+    //android_app_set_motion_event_filter(pApp, motion_event_filter_func);
 
     // This sets up a typical game/event loop. It will run until the app is destroyed.
     do {
@@ -106,16 +122,18 @@ void android_main(struct android_app *pApp) {
         }
 
         // Check if any user data is associated. This is assigned in handle_cmd
-        if (pApp->userData) {
+        if (engine) {
             // We know that our user data is a Renderer, so reinterpret cast it. If you change your
             // user data remember to change it here
-            auto *pRenderer = reinterpret_cast<Renderer *>(pApp->userData);
+            //auto *pRenderer = reinterpret_cast<Renderer *>(pApp->userData);
 
             // Process game input
-            pRenderer->handleInput();
+            //pRenderer->handleInput();
 
             // Render a frame
-            pRenderer->render();
+            //pRenderer->render();
+
+            graphicsService->render();
         }
     } while (!pApp->destroyRequested);
 }
