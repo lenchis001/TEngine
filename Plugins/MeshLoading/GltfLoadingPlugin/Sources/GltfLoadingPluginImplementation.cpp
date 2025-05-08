982064a4eaeff7afd62ebde9a9beeb4d4afa58f4
Liavontsi Khachuyeu <liavontsi_khachuyeu@epam.com>
2025-05-08 11:03:36 +0200

    Refactoring of GLTF plugin

--- Plugins/MeshLoading/GltfLoadingPlugin/Sources/GltfLoadingPluginImplementation.cpp
+++ Plugins/MeshLoading/GltfLoadingPlugin/Sources/GltfLoadingPluginImplementation.cpp
@@ -1,20 +1,11 @@
 #include "GltfLoadingPluginImplementation.h"
 
-#include <iostream>
 #include <sstream>
-#include <string>
-
-#define TINYGLTF_IMPLEMENTATION
-#define STB_IMAGE_IMPLEMENTATION
-#define STB_IMAGE_WRITE_IMPLEMENTATION
-#include "tiny_gltf.h"
 
 #include "boost/filesystem.hpp"
 
 #include "Models/ObjPluginShape.h"
 #include "Models/ObjPluginMesh.h"
-
-using namespace tinygltf;
 
 using namespace ObjLoadingPlugin;
 using namespace ObjLoadingPlugin::Models;
@@ -32,9 +23,100 @@ std::vector<std::string> GltfLoadingPluginImplementation::getSupportedExtensions
     return std::vector<std::string>{"glb"};
 }
 
-std::shared_ptr<IPluginMesh> GltfLoadingPluginImplementation::load(const std::vector<uint8_t> &data, const std::string &basePath)
+std::vector<uint32_t> GltfLoadingPluginImplementation::extractIndices(const Model& model, const Accessor& indexAccessor) const
 {
-    std::istringstream stream(std::string(reinterpret_cast<const char *>(data.data()), data.size()));
+    std::vector<uint32_t> indices;
+    const auto& indexBufferView = model.bufferViews[indexAccessor.bufferView];
+    const auto& indexBuffer = model.buffers[indexBufferView.buffer];
+    const uint16_t* indexData = reinterpret_cast<const uint16_t*>(&indexBuffer.data[indexBufferView.byteOffset]);
+
+    for (size_t i = 0; i < indexAccessor.count; ++i)
+    {
+        indices.push_back(indexData[i]);
+    }
+    return indices;
+}
+
+std::vector<float> GltfLoadingPluginImplementation::extractVertices(const Model& model, const Accessor& positionAccessor, const std::vector<uint32_t>& indices) const
+{
+    std::vector<float> vertices;
+    const auto& positionBufferView = model.bufferViews[positionAccessor.bufferView];
+    const auto& positionBuffer = model.buffers[positionBufferView.buffer];
+    const float* positionData = reinterpret_cast<const float*>(&positionBuffer.data[positionBufferView.byteOffset]);
+
+    for (uint32_t vertexIndex : indices)
+    {
+        vertices.push_back(positionData[vertexIndex * 3 + 0]); // X
+        vertices.push_back(positionData[vertexIndex * 3 + 1]); // Y
+        vertices.push_back(positionData[vertexIndex * 3 + 2]); // Z
+    }
+    return vertices;
+}
+
+std::vector<float> GltfLoadingPluginImplementation::extractNormals(const Model& model, const Accessor& normalAccessor, const std::vector<uint32_t>& indices) const
+{
+    std::vector<float> normals;
+    const auto& normalBufferView = model.bufferViews[normalAccessor.bufferView];
+    const auto& normalBuffer = model.buffers[normalBufferView.buffer];
+    const float* normalData = reinterpret_cast<const float*>(&normalBuffer.data[normalBufferView.byteOffset]);
+
+    for (uint32_t vertexIndex : indices)
+    {
+        normals.push_back(normalData[vertexIndex * 3 + 0]); // X
+        normals.push_back(normalData[vertexIndex * 3 + 1]); // Y
+        normals.push_back(normalData[vertexIndex * 3 + 2]); // Z
+    }
+    return normals;
+}
+
+std::vector<float> GltfLoadingPluginImplementation::extractUVs(const Model& model, const Accessor& uvAccessor, const std::vector<uint32_t>& indices) const
+{
+    std::vector<float> uvs;
+    const auto& uvBufferView = model.bufferViews[uvAccessor.bufferView];
+    const auto& uvBuffer = model.buffers[uvBufferView.buffer];
+    const float* uvData = reinterpret_cast<const float*>(&uvBuffer.data[uvBufferView.byteOffset]);
+
+    for (uint32_t vertexIndex : indices)
+    {
+        uvs.push_back(uvData[vertexIndex * 2 + 0]); // U
+        uvs.push_back(uvData[vertexIndex * 2 + 1]); // V
+    }
+    return uvs;
+}
+
+std::string GltfLoadingPluginImplementation::extractTexturePath(const Model& model, const Material& material, const std::string& basePath) const
+{
+    if (material.pbrMetallicRoughness.baseColorTexture.index >= 0)
+    {
+        const auto& texture = model.textures[material.pbrMetallicRoughness.baseColorTexture.index];
+        if (texture.source >= 0)
+        {
+            const auto& image = model.images[texture.source];
+            if (!image.uri.empty())
+            {
+                return boost::filesystem::absolute(image.uri, basePath).string();
+            }
+            else if (!image.name.empty() && !image.mimeType.empty())
+            {
+                std::string extension;
+                if (image.mimeType == "image/png")
+                {
+                    extension = ".png";
+                }
+                else if (image.mimeType == "image/jpeg")
+                {
+                    extension = ".jpg";
+                }
+                return boost::filesystem::absolute(image.name + extension, basePath).string();
+            }
+        }
+    }
+    return "";
+}
+
+std::shared_ptr<IPluginMesh> GltfLoadingPluginImplementation::load(const std::vector<uint8_t>& data, const std::string& basePath)
+{
+    std::istringstream stream(std::string(reinterpret_cast<const char*>(data.data()), data.size()));
 
     Model model;
     TinyGLTF loader;
@@ -48,71 +130,31 @@ std::shared_ptr<IPluginMesh> GltfLoadingPluginImplementation::load(const std::ve
 
     std::vector<std::shared_ptr<IPluginShape>> shapes;
 
-    for (const auto &mesh : model.meshes)
+    for (const auto& mesh : model.meshes)
     {
-        for (const auto &primitive : mesh.primitives)
+        for (const auto& primitive : mesh.primitives)
         {
             if (primitive.indices < 0)
                 continue;
 
-            std::vector<uint32_t> indices;
-            const auto &indexAccessor = model.accessors[primitive.indices];
-            const auto &indexBufferView = model.bufferViews[indexAccessor.bufferView];
-            const auto &indexBuffer = model.buffers[indexBufferView.buffer];
-            const uint16_t *indexData = reinterpret_cast<const uint16_t *>(&indexBuffer.data[indexBufferView.byteOffset]);
+            const auto& indexAccessor = model.accessors[primitive.indices];
+            std::vector<uint32_t> indices = extractIndices(model, indexAccessor);
 
-            for (size_t i = 0; i < indexAccessor.count; ++i)
-            {
-                indices.push_back(indexData[i]);
-            }
+            const auto& positionAccessor = model.accessors[primitive.attributes.at("POSITION")];
+            std::vector<float> vertices = extractVertices(model, positionAccessor, indices);
 
-            std::vector<float> vertices;
             std::vector<float> normals;
-            std::vector<float> uvs;
-
-            auto floatSize = sizeof(float);
-            const auto &positionAccessor = model.accessors[primitive.attributes.at("POSITION")];
-            const auto &positionBufferView = model.bufferViews[positionAccessor.bufferView];
-            const auto &positionBuffer = model.buffers[positionBufferView.buffer];
-            const float *positionData = reinterpret_cast<const float *>(&positionBuffer.data[positionBufferView.byteOffset]);
-
-            for (size_t i = 0; i < indexAccessor.count; ++i)
-            {
-                uint32_t vertexIndex = indexData[i]; // Индекс вершины
-                vertices.push_back(positionData[vertexIndex * 3 + 0]); // X
-                vertices.push_back(positionData[vertexIndex * 3 + 1]); // Y
-                vertices.push_back(positionData[vertexIndex * 3 + 2]); // Z
-            }
-
             if (primitive.attributes.find("NORMAL") != primitive.attributes.end())
             {
-                const auto &normalAccessor = model.accessors[primitive.attributes.at("NORMAL")];
-                const auto &normalBufferView = model.bufferViews[normalAccessor.bufferView];
-                const auto &normalBuffer = model.buffers[normalBufferView.buffer];
-                const float *normalData = reinterpret_cast<const float *>(&normalBuffer.data[normalBufferView.byteOffset]);
-
-                for (size_t i = 0; i < indexAccessor.count; ++i)
-                {
-                    uint32_t vertexIndex = indexData[i]; // Индекс вершины
-                    normals.push_back(normalData[vertexIndex * 3 + 0]); // X
-                    normals.push_back(normalData[vertexIndex * 3 + 1]); // Y
-                    normals.push_back(normalData[vertexIndex * 3 + 2]); // Z
-                }
+                const auto& normalAccessor = model.accessors[primitive.attributes.at("NORMAL")];
+                normals = extractNormals(model, normalAccessor, indices);
             }
 
+            std::vector<float> uvs;
             if (primitive.attributes.find("TEXCOORD_0") != primitive.attributes.end())
             {
-                const auto &uvAccessor = model.accessors[primitive.attributes.at("TEXCOORD_0")];
-                const auto &uvBufferView = model.bufferViews[uvAccessor.bufferView];
-                const auto &uvBuffer = model.buffers[uvBufferView.buffer];
-                const float *uvData = reinterpret_cast<const float *>(&uvBuffer.data[uvBufferView.byteOffset]);
-
-                for (size_t i = 0; i < indexAccessor.count; ++i)
-                {
-                    uint32_t vertexIndex = indexData[i]; // Индекс вершины
-                    uvs.push_back(uvData[vertexIndex * 2 + 0]); // U
-                    uvs.push_back(uvData[vertexIndex * 2 + 1]); // V
-                }
+                const auto& uvAccessor = model.accessors[primitive.attributes.at("TEXCOORD_0")];
+                uvs = extractUVs(model, uvAccessor, indices);
             }
 
             std::vector<float> diffuseColor = {0.f, 0.f, 0.f};
@@ -120,40 +162,8 @@ std::shared_ptr<IPluginMesh> GltfLoadingPluginImplementation::load(const std::ve
 
             if (primitive.material >= 0)
             {
-                const auto &material = model.materials[primitive.material];
-                if (material.pbrMetallicRoughness.baseColorFactor.size() == 4)
-                {
-                    // diffuseColor[0] = static_cast<float>(material.pbrMetallicRoughness.baseColorFactor[0]);
-                    // diffuseColor[1] = static_cast<float>(material.pbrMetallicRoughness.baseColorFactor[1]);
-                    // diffuseColor[2] = static_cast<float>(material.pbrMetallicRoughness.baseColorFactor[2]);
-                }
-
-                if (material.pbrMetallicRoughness.baseColorTexture.index >= 0)
-                {
-                    const auto &texture = model.textures[material.pbrMetallicRoughness.baseColorTexture.index];
-                    if (texture.source >= 0)
-                    {
-                        const auto &image = model.images[texture.source];
-                        if (!image.uri.empty())
-                        {
-                            texturePath = boost::filesystem::absolute(image.uri, basePath).string();
-                        }
-                        else if (!image.name.empty() && !image.mimeType.empty())
-                        {
-                            std::string extension;
-                            if (image.mimeType == "image/png")
-                            {
-                                extension = ".png";
-                            }
-                            else if (image.mimeType == "image/jpeg")
-                            {
-                                extension = ".jpg";
-                            }
-                            // Add more mime types if needed
-                            texturePath = boost::filesystem::absolute(image.name + extension, basePath).string();
-                        }
-                    }
-                }
+                const auto& material = model.materials[primitive.material];
+                texturePath = extractTexturePath(model, material, basePath);
             }
 
             shapes.push_back(std::make_shared<ObjPluginShape>(
--- Plugins/MeshLoading/GltfLoadingPlugin/Sources/GltfLoadingPluginImplementation.h
+++ Plugins/MeshLoading/GltfLoadingPlugin/Sources/GltfLoadingPluginImplementation.h
@@ -1,7 +1,17 @@
 #ifndef OBJ_LOADING_PLUGIN_H
 #define OBJ_LOADING_PLUGIN_H
 
+#include <vector>
+#include <string>
+
+#define TINYGLTF_IMPLEMENTATION
+#define STB_IMAGE_IMPLEMENTATION
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "tiny_gltf.h"
+
 #include "IMeshLoadingPlugin.h"
+
+using namespace tinygltf;
 
 using namespace TEngine::Components::Graphics::MeshLoading::Plugin;
 
@@ -16,6 +26,13 @@ namespace ObjLoadingPlugin
         virtual std::vector<std::string> getSupportedExtensions() const override;
 
         virtual std::shared_ptr<IPluginMesh> load(const std::vector<uint8_t>& data, const std::string& basePath) override;
+
+    private:
+        std::vector<uint32_t> extractIndices(const tinygltf::Model& model, const tinygltf::Accessor& indexAccessor) const;
+        std::vector<float> extractVertices(const tinygltf::Model& model, const tinygltf::Accessor& positionAccessor, const std::vector<uint32_t>& indices) const;
+        std::vector<float> extractNormals(const tinygltf::Model& model, const tinygltf::Accessor& normalAccessor, const std::vector<uint32_t>& indices) const;
+        std::vector<float> extractUVs(const tinygltf::Model& model, const tinygltf::Accessor& uvAccessor, const std::vector<uint32_t>& indices) const;
+        std::string extractTexturePath(const tinygltf::Model& model, const tinygltf::Material& material, const std::string& basePath) const;
     };
 }
 
